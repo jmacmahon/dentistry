1dfc233602db43d99d79c34fea51d6f1ea436993
Adam Phillips <adamphillips3@sheffield.ac.uk>
2016-11-25 19:10:04 +0000

    search on patients page

--- Dentistry/src/views/Patients.java
+++ Dentistry/src/views/Patients.java
@@ -21,6 +21,8 @@ public class Patients extends ViewComponent {
 		JPanel panel = new JPanel();
 		panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
 		panel.add((new Buttons()).getPanel());
+		panel.add(new SearchPatients().getPanel());
+		panel.add(new SearchButton().getPanel());
 		for (PatientInterface patient : this.patients) {
 			// TODO maybe scrolling?
 			panel.add(new JSeparator(JSeparator.HORIZONTAL));
@@ -34,7 +36,6 @@ public class Patients extends ViewComponent {
 		public JPanel getPanel() {
 			JPanel panel = new JPanel();
 			panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
-			JButton search = new JButton("Search patients");
 			JButton newPatient = new JButton("Register new patient");
 			newPatient.addActionListener(e -> {
 				ViewComponent.spawnInFrame(new NewPatient(), "New Patient");
@@ -43,5 +44,39 @@ public class Patients extends ViewComponent {
 			panel.add(newPatient);
 			return panel;
 		}
+	}
+	
+	private class SearchText implements ViewComponent {
+		@Override
+		public JPanel getPanel() {
+			JPanel panel = new JPanel();
+			panel.setLayout(new GridLayout(0,2));
+			
+			JTextField houseNumber = new JTextField("House Number");
+			JTextField postcode = new JTextField("Postcode");
+			
+			panel.add(new JLabel("House Number: "));
+			panel.add(houseNumber);
+			
+			panel.add(new JLabel("Postcode: "));
+			panel.add(postcode);
+			
+			return panel;
+			
+		}
+	}
+	
+private class SearchButton implements ViewComponent {
+		
+		public JPanel getPanel() {
+			JPanel panel = new JPanel();
+			
+			JButton search = new JButton("Search patients");
+		
+			panel.add(search);
+			
+			return panel;
+		}
+	
 	}
 }
